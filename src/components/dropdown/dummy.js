e4e68255d1a8c846127f523b8456b6f5674509ee
aziz-codes <azizisoffline@gmail.com>
2023-11-24 23:08:14 +0500

    created a dummy data set to render

---
+++ src/components/dropdown/dummy.js
@@ -0,0 +1,34 @@
+export const languages = [
+  {
+    label: "C++",
+    value: "C++",
+  },
+  {
+    label: "Java",
+    value: "Java",
+  },
+  {
+    label: "C#",
+    value: "C#",
+  },
+  {
+    label: "SQL",
+    value: "SQL",
+  },
+  {
+    label: "PHP",
+    value: "PHP",
+  },
+  {
+    label: "Dart",
+    value: "Dart",
+  },
+  {
+    label: "Python",
+    value: "Python",
+  },
+  {
+    label: "JavaScript",
+    value: "JavaScript",
+  },
+];

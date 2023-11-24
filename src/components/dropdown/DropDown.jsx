85f2fd0bbf8c31229084e507e0fb92c79f606a2d
aziz-codes <azizisoffline@gmail.com>
2023-11-25 00:37:15 +0500

    added window click listener to detect click

--- src/components/dropdown/DropDown.jsx
+++ src/components/dropdown/DropDown.jsx
@@ -1,50 +1,61 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import Input from "./Input";
 import { languages } from "./dummy";
 import { ChevronDownIcon } from "@heroicons/react/24/outline";
 const DropDown = () => {
-  const [focused, setFocused] = useState(false);
-  const [value, setValue] = useState("");
-  const handleFocus = () => {
-    setFocused(true);
+  const [clicked, setClicked] = useState(false);
+  const [selected, setSelected] = useState("");
+  const InputRef = useRef(null);
+
+  useEffect(() => {
+    const handleWindowClick = (e) => {
+      if (InputRef.current.contains(e.target)) {
+        setClicked(true);
+      } else {
+        setClicked(false);
+      }
+    };
+    window.addEventListener("click", handleWindowClick);
+
+    return () => window.removeEventListener("click", handleWindowClick);
+  }, []);
+
+  const handleClick = (item) => {
+    setSelected(item);
+    setClicked(false);
   };
-  const handleBlur = () => {
-    setFocused(false);
-  };
+  console.log(clicked);
   return (
     <section className="h-screen w-full flex justify-center mt-20">
       <div className="w-full max-w-sm p-5">
-        <div className="flex h-8  rounded-md w-full border items-center px-1 relative">
-          <Input
-            handleFocus={handleFocus}
-            handleBlur={handleBlur}
-            value={value}
-          />
+        <div className="flex h-8  rounded-md w-full border items-center px-1 relative options  ">
+          <div ref={InputRef} className="w-full ">
+            <Input selected={selected} />
+          </div>
           <ChevronDownIcon
-            className={`h-5 w-5 text-gray-400 cursor-pointer ${
-              focused && "rotate-180"
+            className={`h-5 w-5 text-gray-400 cursor-pointer transition-transform duration-200 ease-linear ${
+              clicked &&
+              "rotate-180 transition-transform duration-200 ease-linear"
             }`}
-            onClick={() => setFocused((prev) => !prev)}
+            onClick={() => setClicked((prev) => !prev)}
           />
-          {focused ? (
+
+          {clicked && (
             <div
               className="absolute left-0 top-8 w-full h-auto px-0.5 
-          flex flex-col gap-1 border border-t-0 shadow-md"
+          flex flex-col gap-1 border border-t-0 shadow-md "
             >
               {languages.map((item, index) => (
                 <div
                   className="bg-white px-1 py-1 cursor-pointer hover:bg-slate-100 transition-colors duration-150 ease-linear"
                   key={index}
-                  onClick={() => {
-                    setValue(item.value);
-                    setFocused(false);
-                  }}
+                  onClick={() => handleClick(item.value)}
                 >
                   {item.label}
                 </div>
               ))}
             </div>
-          ) : null}
+          )}
         </div>
       </div>
     </section>
